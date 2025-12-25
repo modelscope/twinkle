f16e153dc19681cd34937c26ad81cce4fe8b934d
tastelikefeet <yuze.zyz@alibaba-inc.com>
2025-12-25 14:46:09 +0800

    wip

--- src/twinkle/processor/base.py
+++ src/twinkle/processor/base.py
@@ -3,4 +3,10 @@
 class InputProcessor:
 
     def __call__(self, inputs):
+        return self.prepare_task_inputs(self.redistribute(inputs))
+
+    def redistribute(self, inputs):
+        return inputs
+
+    def prepare_task_inputs(self, inputs):
         return inputs
