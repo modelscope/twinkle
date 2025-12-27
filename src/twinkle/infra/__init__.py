a054d6f984feda4377a077892551e84e4232552f
tastelikefeet <yuze.zyz@alibaba-inc.com>
2025-12-27 13:05:55 +0800

    wip

--- src/twinkle/infra/__init__.py
+++ src/twinkle/infra/__init__.py
@@ -126,34 +126,6 @@ def dispatch_args(workers, dispatch, execute, device_mesh: DeviceMesh, args, kwa
         raise ValueError(f'Unsupported dispatch method: {dispatch}')
 
 
-def dispatch(self, inputs):
-    if self.device_mesh is None:
-        return inputs
-
-    dp_rank = self.device_mesh.dp_rank
-    dp_world_size = self.device_mesh.dp_world_size
-
-    total_items = len(inputs)
-    if total_items < dp_world_size:
-        last_item = inputs[-1] if inputs else None
-        inputs = inputs + [last_item] * (dp_world_size - total_items)
-        total_items = dp_world_size
-
-    # Calculate data slice for current rank
-    items_per_rank = total_items // dp_world_size
-    remainder = total_items % dp_world_size
-
-    # Distribute remainder items to first 'remainder' ranks
-    if dp_rank < remainder:
-        start_idx = dp_rank * (items_per_rank + 1)
-        end_idx = start_idx + items_per_rank + 1
-    else:
-        start_idx = remainder * (items_per_rank + 1) + (dp_rank - remainder) * items_per_rank
-        end_idx = start_idx + items_per_rank
-
-    return inputs[start_idx:end_idx]
-
-
 def remote_class():
 
     def decorator(cls):
