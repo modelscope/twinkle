66a9ec9b810bfb4e0f2bde00e022ecdc0f75f4e4
tastelikefeet <yuze.zyz@alibaba-inc.com>
2026-01-07 18:21:28 +0800

    wip

--- src/twinkle/model/multi_lora_transformers.py
+++ src/twinkle/model/multi_lora_transformers.py
@@ -45,27 +45,35 @@ class MultiLoraTransformersModel(TransformersModel, PreTrainedModel):
     def _check_adapter_valid(self, adapter_name: str):
         assert adapter_name and adapter_name in self.optimizer_group, f'Use a valid {adapter_name} first, current is: {adapter_name}'
 
+    def _activate_adapter(self, adapter_name: str):
+        model = self.strategy.unwrap_model(self.model)
+        model.set_current_adapter_name(adapter_name)
+
     def _lazy_wrap_model(self):
         pass
 
     @remote_function()
     def forward(self, *, inputs: Union[InputFeature, List[InputFeature], Trajectory, List[Trajectory]], **kwargs):
         self._check_adapter_valid(kwargs.get("adapter_name"))
+        self._activate_adapter(kwargs.get("adapter_name"))
         return super().forward(inputs, **kwargs)
 
     @remote_function()
     def forward_only(self, *, inputs: Union[InputFeature, List[InputFeature], List[Trajectory]], **kwargs):
         self._check_adapter_valid(kwargs.get("adapter_name"))
+        self._activate_adapter(kwargs.get("adapter_name"))
         return super().forward_only(inputs, **kwargs)
 
     @remote_function()
     def calculate_loss(self, **kwargs):
         self._check_adapter_valid(kwargs.get("adapter_name"))
+        self._activate_adapter(kwargs.get("adapter_name"))
         return super().calculate_loss(**kwargs)
 
     @remote_function()
     def backward(self, **kwargs):
         self._check_adapter_valid(kwargs.get("adapter_name"))
+        self._activate_adapter(kwargs.get("adapter_name"))
         super().backward(**kwargs)
         self._reduce_adapter_grad(adapter_name=kwargs.get("adapter_name"))
 
@@ -89,38 +97,55 @@ class MultiLoraTransformersModel(TransformersModel, PreTrainedModel):
     @remote_function()
     def clip_grad_norm(self, max_grad_norm: float=1.0, norm_type=2, **kwargs):
         self._check_adapter_valid(kwargs.get("adapter_name"))
+        self._activate_adapter(kwargs.get("adapter_name"))
         return super().clip_grad_norm(max_grad_norm, norm_type=norm_type, **kwargs)
 
     @remote_function()
     def step(self, **kwargs):
         self._check_adapter_valid(kwargs.get("adapter_name"))
+        self._activate_adapter(kwargs.get("adapter_name"))
         super().step(**kwargs)
 
     @remote_function()
     def zero_grad(self, **kwargs):
         self._check_adapter_valid(kwargs.get("adapter_name"))
+        self._activate_adapter(kwargs.get("adapter_name"))
         super().zero_grad(**kwargs)
 
     @remote_function()
     def lr_step(self, **kwargs):
         self._check_adapter_valid(kwargs.get("adapter_name"))
+        self._activate_adapter(kwargs.get("adapter_name"))
         super().lr_step(**kwargs)
 
     @remote_function()
     def set_loss(self, loss_cls: Union[Type[Loss], str], **kwargs):
         self._check_adapter_valid(kwargs.get("adapter_name"))
+        self._activate_adapter(kwargs.get("adapter_name"))
         super().set_loss(loss_cls, **kwargs)
 
     @remote_function()
     def set_optimizer(self, optimizer_cls: Union[Type[Optimizer], str], **kwargs):
         self._check_adapter_valid(kwargs.get("adapter_name"))
+        self._activate_adapter(kwargs.get("adapter_name"))
         super().set_optimizer(optimizer_cls, **kwargs)
 
     def add_adapter_to_model(self, adapter_name: str, config_or_dir: Union[PeftConfig, str], **kwargs):
-        self._patch_adapter(adapter_name, config_or_dir, _default_adapter_name, **kwargs)
+        # prevent opening requires_grad of the base model
+        # prevent loading malicious code
+        assert not isinstance(config_or_dir, str), 'config_or_dir does not support str, because loading config from modelhub may causing unexpected behavior'
+        assert isinstance(config_or_dir, LoraConfig), 'config_or_dir must be a LoraConfig instance'
+        # Limit the max peft version in pyproject.toml, in case any newer version opens some untested module grad.
+        config_or_dir.modules_to_save = None
+        config_or_dir.bias = 'none'
+        config_or_dir.init_lora_weights = False
+        config_or_dir.modules_to_save = None
+        config_or_dir.trainable_token_indices = None
+        self._patch_adapter(adapter_name, config_or_dir, adapter_name, **kwargs)
         self._activate_adapter(adapter_name)
+        self._prepare_adapter(adapter_name)
 
-    def _activate_adapter(self, adapter_name: str):
+    def _prepare_adapter(self, adapter_name: str):
         self._check_adapter_valid(adapter_name)
         pattern = re.compile(r'\.lora_\w+\.[^.]+\.')
         unwrapped_model = self.strategy.unwrap_model(self.model)
@@ -132,30 +157,31 @@ class MultiLoraTransformersModel(TransformersModel, PreTrainedModel):
     @remote_function()
     def set_lr_scheduler(self, scheduler_cls: Union[Type[LRScheduler], str], **kwargs):
         self._check_adapter_valid(kwargs.get("adapter_name"))
+        self._activate_adapter(kwargs.get("adapter_name"))
         super().set_lr_scheduler(scheduler_cls, **kwargs)
-
-    @remote_function()
-    def add_adapter_to_model(self, adapter_name: str, config_or_dir: Union[PeftConfig, str], **kwargs):
-        super()._patch_adapter(adapter_name, config_or_dir, adapter_name, **kwargs)
 
     @remote_function()
     def set_template(self, template_cls: Union[Type[template.Template], str], **kwargs):
         self._check_adapter_valid(kwargs.get("adapter_name"))
+        self._activate_adapter(kwargs.get("adapter_name"))
         super().set_template(template_cls, **kwargs)
 
     @remote_function()
     def set_processor(self, processor_cls: Union[Type[InputProcessor], str], **kwargs):
         self._check_adapter_valid(kwargs.get("adapter_name"))
+        self._activate_adapter(kwargs.get("adapter_name"))
         super().set_processor(processor_cls, **kwargs)
 
     @remote_function()
     def set_grad_scaler(self, **kwargs):
         self._check_adapter_valid(kwargs.get("adapter_name"))
+        self._activate_adapter(kwargs.get("adapter_name"))
         super().set_grad_scaler(**kwargs)
 
     @remote_function()
     def remove_adapter(self, adapter_name: str):
         if adapter_name in self.optimizer_group:
             self.optimizer_group.pop(adapter_name)
-        if isinstance(self.model, PeftModel):
-            self.model.delete_adapter(adapter_name)
+        model = self.strategy.unwrap_model(self.model)
+        if isinstance(model, PeftModel):
+            model.base_model.delete_adapter(adapter_name=adapter_name)
--- src/twinkle/patch/multi_adapter.py
+++ src/twinkle/patch/multi_adapter.py
@@ -1,6 +1,6 @@
 import contextvars
 
-from peft.tuners.lora import LoraLayer
+from peft.tuners.lora import LoraLayer, LoraModel
 
 from .base import Patch
 
@@ -13,7 +13,10 @@ class MultiAdapter(Patch):
         if getattr(LoraLayer, '_patched', False):
             return module
 
-        def get_active_adapters(_):
+        def get_active_adapter(*args, **kwargs):
+            return MultiAdapter._adapter_var.get()
+
+        def get_active_adapters(*args, **kwargs):
             adapter_name = MultiAdapter._adapter_var.get()
             if adapter_name:
                 return [adapter_name]
@@ -23,9 +26,26 @@ class MultiAdapter(Patch):
         def set_active_adapters(_, value):
             pass
 
-        LoraLayer.active_adapters = property(get_active_adapters, set_active_adapters)
-        LoraLayer._patched = True
+        def set_adapter(self, adapter_names):
+            pass
 
+        LoraLayer.active_adapter = property(get_active_adapter, set_active_adapters)
+        LoraLayer.active_adapters = property(get_active_adapters, set_active_adapters)
+        LoraLayer.set_adapter = set_adapter
+        LoraLayer._patched = True
+        LoraModel.active_adapter = property(get_active_adapter, set_active_adapters)
+        LoraModel.active_adapters = property(get_active_adapters, set_active_adapters)
+        LoraModel.set_adapter = set_adapter
+        LoraModel._patched = True
+
+        def _mark_only_adapters_as_trainable(self, model) -> None:
+            for n, p in model.named_parameters():
+                if "lora_" not in n:
+                    p.requires_grad = False
+                else:
+                    p.requires_grad = True
+
+        LoraModel._mark_only_adapters_as_trainable = _mark_only_adapters_as_trainable
         module.set_current_adapter_name = MultiAdapter.set_current_adapter_name
         return module
 
