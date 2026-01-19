199f9befdd205bc5ac69beed6d858f37bc41d67c
tastelikefeet <yuze.zyz@alibaba-inc.com>
2026-01-19 16:57:30 +0800

    wip

--- src/twinkle/dataset/__init__.py
+++ src/twinkle/dataset/__init__.py
@@ -1,6 +1,5 @@
 # Copyright (c) ModelScope Contributors. All rights reserved.
 from .base import Dataset, DatasetMeta
-from .iterable_dataset import IterableDataset
 from .lazy_dataset import LazyDataset
 from .iterable_packing_dataset import IterablePackingDataset
 from .packing_dataset import PackingDataset
