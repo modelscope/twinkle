fe108a8400b13dd4a769081cd7cb19ee2d085963
addsubmuldiv <zyh13227@163.com>
2026-02-02 07:58:58 +0000

    optimize

--- cookbook/remote/twinkle/server.py
+++ cookbook/remote/twinkle/server.py
@@ -31,12 +31,6 @@ for app_config in config.applications:
     builder = APP_BUILDERS[app_config.import_path]
     args = OmegaConf.to_container(app_config.args, resolve=True) if app_config.args else {}
 
-    # Ray Serve replica processes do not reliably inherit the launching script's env vars.
-    # Therefore explicitly inject the visible devices into the device_group config so Twinkle can set visibility inside each worker.
-    visible_devices = os.environ.get('ASCEND_RT_VISIBLE_DEVICES') or os.environ.get('CUDA_VISIBLE_DEVICES')
-    if visible_devices and 'device_group' in args:
-        args['device_group']['visible_devices'] = visible_devices
-
     deploy_options = {}
     deploy_config = app_config.deployments[0]
     if 'autoscaling_config' in deploy_config:
--- src/twinkle/server/tinker/__init__.py
+++ src/twinkle/server/tinker/__init__.py
@@ -1,3 +1,7 @@
 # Copyright (c) ModelScope Contributors. All rights reserved.
-from .model import build_model_app
+from .model import build_model_app as _build_model_app
 from .server import build_server_app
+from ..utils import wrap_builder_with_device_group_env
+
+
+build_model_app = wrap_builder_with_device_group_env(_build_model_app)
