51cc9bad5a38ad7848afcef741e813cc216a7d75
Yunnglin <mao.looper@qq.com>
2026-02-03 15:09:02 +0800

    update queue

--- src/twinkle/server/tinker/model.py
+++ src/twinkle/server/tinker/model.py
@@ -231,10 +231,6 @@ def build_model_app(model_id: str,
                             self.adapter_records[adapter_name] = 0
                         self.model.set_processor('InputProcessor',
                                                  adapter_name=adapter_name)
-                        # When use_megatron is True, we don't need to set the loss
-                        if not self.use_megatron:
-                            self.model.set_loss('CrossEntropyLoss',
-                                            adapter_name=adapter_name)
                         self.model.set_optimizer('Adam',
                                                  adapter_name=adapter_name)
 
