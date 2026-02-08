b69b88861078a025052a8f18f83e00eb582174fc
Yunnglin <maoyl@smail.nju.edu.cn>
2026-02-08 22:41:42 +0800

    update

--- src/twinkle/server/twinkle/common/serialize.py
+++ src/twinkle/server/twinkle/common/serialize.py
@@ -23,10 +23,38 @@ container_types = (Mapping, list, tuple, set, frozenset)
 basic_types = (*primitive_types, *container_types)
 
 
+def _serialize_data_slice(data_slice):
+    """Serialize data_slice (Iterable) into a JSON-compatible dict."""
+    if data_slice is None:
+        return None
+    if isinstance(data_slice, range):
+        return {'_slice_type_': 'range', 'start': data_slice.start, 'stop': data_slice.stop, 'step': data_slice.step}
+    if isinstance(data_slice, (list, tuple)):
+        return {'_slice_type_': 'list', 'values': list(data_slice)}
+    raise ValueError(
+        f'Http mode does not support data_slice of type {type(data_slice).__name__}. '
+        'Supported types: range, list, tuple.'
+    )
+
+
+def _deserialize_data_slice(data_slice):
+    """Deserialize a dict back into the original data_slice object."""
+    if data_slice is None:
+        return None
+    if not isinstance(data_slice, dict) or '_slice_type_' not in data_slice:
+        return data_slice
+    slice_type = data_slice['_slice_type_']
+    if slice_type == 'range':
+        return range(data_slice['start'], data_slice['stop'], data_slice['step'])
+    if slice_type == 'list':
+        return data_slice['values']
+    raise ValueError(f'Unsupported data_slice type: {slice_type}')
+
+
 def serialize_object(obj) -> str:
     if isinstance(obj, DatasetMeta):
-        assert obj.data_slice is None, 'Http mode does not support data_slice'
-        data = obj.__dict__
+        data = obj.__dict__.copy()
+        data['data_slice'] = _serialize_data_slice(data.get('data_slice'))
         data['_TWINKLE_TYPE_'] = 'DatasetMeta'
         return json.dumps(data, ensure_ascii=False)
     elif isinstance(obj, LoraConfig):
@@ -54,6 +82,7 @@ def deserialize_object(data: str) -> Any:
     if '_TWINKLE_TYPE_' in data:
         _type = data.pop('_TWINKLE_TYPE_')
         if _type == 'DatasetMeta':
+            data['data_slice'] = _deserialize_data_slice(data.get('data_slice'))
             return DatasetMeta(**data)
         elif _type == 'LoraConfig':
             return LoraConfig(**data)
