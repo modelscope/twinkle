7c17844e329237f1aa4d4ef2221e9ba05ebb0d80
tastelikefeet <yuze.zyz@alibaba-inc.com>
2026-02-13 15:40:05 +0800

    lint code

--- tests/sampler/test_sampler_e2e.py
+++ tests/sampler/test_sampler_e2e.py
@@ -20,10 +20,9 @@ Environment:
 
 import argparse
 import os
+import pytest
 import sys
 import traceback
-
-import pytest
 
 # Set environment variables before imports
 os.environ.setdefault('TRUST_REMOTE_CODE', '1')
