09ca8f4385a088286406f42acf3a14162ce3af0b
Yunnglin <mao.looper@qq.com>
2026-03-13 23:52:21 +0800

    update

--- src/twinkle_client/http/__init__.py
+++ src/twinkle_client/http/__init__.py
@@ -1,4 +1,3 @@
-from .heartbeat import heartbeat_manager
 from .http_utils import http_delete, http_get, http_post
 from .utils import (TWINKLE_SERVER_TOKEN, TWINKLE_SERVER_URL, get_api_key, get_base_url, get_request_id,
                     get_session_id, set_api_key, set_base_url, set_request_id, set_session_id)
@@ -7,7 +6,6 @@ __all__ = [
     'http_get',
     'http_post',
     'http_delete',
-    'heartbeat_manager',
     'TWINKLE_SERVER_URL',
     'TWINKLE_SERVER_TOKEN',
     'set_base_url',
