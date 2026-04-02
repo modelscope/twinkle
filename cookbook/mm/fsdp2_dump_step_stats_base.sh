2538440889a84bfb6a6fb4a1203f719d93a26994
meichangsu1 <1484603386@qq.com>
2026-04-02 17:57:17 +0800

    wip

--- cookbook/mm/fsdp2_dump_step_stats_base.sh
+++
@@ -1,5 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,1 \
-TWINKLE_COMPARE_SEED=1234 \
-TWINKLE_COMPARE_STEP=0 \
-TWINKLE_COMPARE_OUTPUT=compare_step_stats_base.pt \
-torchrun --nproc_per_node=2 fsdp2_dump_step_stats.py
